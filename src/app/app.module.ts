cd98cc7f766a437a663a5366bcb9c3c6b15b61e5
rohit nagappa <rnagappa35@gmail.com>
2020-11-04 09:48:55 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    QuizComponent,
+    ResultComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
