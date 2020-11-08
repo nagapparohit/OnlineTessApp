22a3ac8396acd1e7d2c8c156b272133aa3c4a6e3
rohit nagappa <rnagappa35@gmail.com>
2020-11-08 10:17:47 +0530

    added review

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 
 @Component({
   selector: 'app-quiz',
@@ -15,6 +16,8 @@ export class QuizComponent implements OnInit {
   public follow:number = 0;
 
   private solutions = new Map();
+
+  public result:string = "";
 
   public question = {
     "id":undefined,
@@ -37,14 +40,16 @@ export class QuizComponent implements OnInit {
   }
 
   nextQuestion(nextButton,a,b,c,d){
+    console.log(this.solutions);
+    console.log("follow value is "+this.follow);
     this.follow++;
     if(this.follow < this.questionsList.length){
       console.log((this.questionsList[this.follow]).ques);
-      nextButton.innerText = this.questionsList[this.follow].ques;
-      a.innerText  = this.questionsList[this.follow].a;
-      b.innerText = this.questionsList[this.follow].b;
-      c.innerText = this.questionsList[this.follow].c;
-      d.innerText = this.questionsList[this.follow].d;    
+      nextButton.innerText = " Q. "+this.questionsList[this.follow].ques;
+      a.innerText  = " A. "+this.questionsList[this.follow].a;
+      b.innerText = " B. "+this.questionsList[this.follow].b;
+      c.innerText = " C. "+this.questionsList[this.follow].c;
+      d.innerText = " D. "+this.questionsList[this.follow].d;    
       console.log(nextButton);
 
     }else{
@@ -58,6 +63,40 @@ export class QuizComponent implements OnInit {
 
   radioChangeHandler(radioButton){
     console.log(radioButton.target.value);
+    this.solutions.set(this.questionsList[this.follow].id,radioButton.target.value);
+  }
+
+  startQuiz(startButton,a,b,c,d){
+
+      startButton.innerText = " Q. "+this.questionsList[this.follow].ques;
+      a.innerText  = " A. "+this.questionsList[this.follow].a;
+      b.innerText = " B. "+this.questionsList[this.follow].b;
+      c.innerText = " C. "+this.questionsList[this.follow].c;
+      d.innerText = " D. "+this.questionsList[this.follow].d;
+  
+    }
+
+  submitAction(){
+
+    let  answerSource = 'assets/answers.json';
+    let answerList;
+    let result :number = 0;
+    this.httpClient.get(answerSource).subscribe(data =>{
+      console.log(data);
+      console.log(this.solutions);
+      answerList = data;
+    },err => {
+      console.log(err);
+    })
+ 
+    for(var val of this.solutions ){
+      console.log(val.keys);
+     this.result = "Result " + "2/3";
+    }
+  }
+
+  reviewAction(){
+
   }
 
 }
